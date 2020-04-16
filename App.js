61c6ed3dd1bcad840dcc1da3b71047260917b3b5
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-16 19:59:58 +0400

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Goal" style={styles.input} />
+                <Button title="ADD" />
+            </View>
+            <View />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10
+    }
+});
