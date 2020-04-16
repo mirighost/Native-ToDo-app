0728192f43519ec9744da457feefde96f06517e6
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-16 21:19:10 +0400

    added functions to add items to list

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
 
 export default function App() {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [goals, setGoals] = useState([]);
+
+    // New item
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText)
+    };
+
+    // Adding items to list
+    const addGoalHandler = () => {
+        setGoals(currentGoals => [...currentGoals, enteredGoal]);
+        setEnteredGoal('')
+    };
+
     return (
         <View style={styles.screen}>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="Goal" style={styles.input} />
-                <Button title="ADD" />
+
+                <TextInput
+                    placeholder="Goal"
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+
+                <Button title="ADD" onPress={addGoalHandler} />
+
             </View>
-            <View />
+            <View>
+                {goals.map((goal) => <Text key={goal}>{goal}</Text>)}
+            </View>
         </View>
     );
 };
 
+// Styles
 const styles = StyleSheet.create({
     screen: {
         padding: 50
@@ -24,7 +49,7 @@ const styles = StyleSheet.create({
     },
     input: {
         width: '80%',
-        borderColor: 'black',
+        borderBottomColor: "black",
         borderWidth: 1,
         padding: 10
     }
